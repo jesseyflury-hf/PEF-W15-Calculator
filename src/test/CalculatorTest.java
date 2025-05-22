d1fb98a0ab4dad86cb008717b923c75510c5c1fe
jesseyflury-hf <j.flury@hf-ict.info>
2025-05-22 13:12:59 +0200

    Create CalculatorTest.java

---
+++ src/test/CalculatorTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sum() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mult() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void div() {
+    }
+}
